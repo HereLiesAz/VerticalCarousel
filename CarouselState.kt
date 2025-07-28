1371c26ab36d9bd69122e84f816758232f6e916f
Az <103241502+HereLiesAz@users.noreply.github.com>
2025-07-27 22:34:47 -0500

    Update CarouselState.kt

--- CarouselState.kt
+++ CarouselState.kt
@@ -1,10 +1,13 @@
 package com.hereliesaz.verticalcarousel
 
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.gestures.FlingBehavior
 import androidx.compose.foundation.gestures.ScrollableState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.unit.Density
+import com.hereliesaz.verticalcarousel.internal.CarouselFlingBehavior
+import com.hereliesaz.verticalcarousel.internal.CarouselScrollableState
 import com.hereliesaz.verticalcarousel.internal.KeylineState
 
 @Composable
@@ -25,8 +28,15 @@ class CarouselState(
     val itemCount: () -> Int,
 ) {
     internal lateinit var keylineState: KeylineState
-    internal lateinit var scrollableState: ScrollableState
-    internal lateinit var flingBehavior: FlingBehavior
-    internal lateinit var density: Density
+    internal val scrollableState: ScrollableState = CarouselScrollableState { delta ->
+        keylineState.scrollBy(delta)
+    }
 
+    internal fun fling(velocity: Float, spec: AnimationSpec<Float>): FlingBehavior {
+        return CarouselFlingBehavior(
+            scrollableState = scrollableState,
+            keylineState = keylineState,
+            snapAnimationSpec = spec
+        )
+    }
 }
