81b03136a5c6cd0865db8188f023632f5bf3c3e0
Az <103241502+HereLiesAz@users.noreply.github.com>
2025-07-27 21:52:38 -0500

    Update VerticalUncontainedCarousel.kt

--- VerticalUncontainedCarousel.kt
+++ VerticalUncontainedCarousel.kt
@@ -1,14 +1,18 @@
 package com.hereliesaz.verticalcarousel
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
+import kotlin.math.absoluteValue
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -26,6 +30,22 @@ fun VerticalUncontainedCarousel(
         pageSize = PageSize.Fixed(itemHeight),
         pageSpacing = itemSpacing,
         contentPadding = contentPadding,
-        content = { page -> content(page) }
-    )
+    ) { page ->
+        val pageOffset = (
+            (state.currentPage - page) + state.currentPageOffsetFraction
+            ).absoluteValue
+
+        val scale = lerp(1f, 0.9f, pageOffset.coerceIn(0f, 1f))
+        val alpha = lerp(1f, 0.5f, pageOffset.coerceIn(0f, 1f))
+
+        Box(
+            modifier = Modifier.graphicsLayer {
+                scaleX = scale
+                scaleY = scale
+                this.alpha = alpha
+            }
+        ) {
+            content(page)
+        }
+    }
 }
